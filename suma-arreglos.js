a34000161069510df93fb6cf5327f9c5e29cd1cb
wey-monsalve <wmarmonsa@gmail.com>
2022-11-10 13:11:18 -0500

    sumando los numeros del arreglo

---
+++ suma-arreglos.js
@@ -0,0 +1,11 @@
+// para sumar la cantidad el total que me da los numeros que tengo en el arreglo
+
+let numeros =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+let i = numeros.length
+let suma = 0
+
+
+for (let i = 0; i < numeros.length; i++) {
+    suma = suma + numeros[i]
+}
+    console.log(suma)  
