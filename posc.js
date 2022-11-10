967e6ef2b574c2de2c98bde3733e1de9c529b5a4
wey-monsalve <wmarmonsa@gmail.com>
2022-11-10 12:28:49 -0500

    ejemplos de trabajar con arreglos

---
+++ posc.js
@@ -0,0 +1,129 @@
+//para mostrar cantidad de numeros del array
+
+let numeros =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+let i = numeros.length
+let suma = 0
+
+//console.log(i)  con este se muestra la cantidad de los numeros
+
+//para mostrar cada numero del array
+
+//for (let i = 0; i < numeros.length; i++) {
+  //  console.log(numeros[i])  con este mostramos cada numero
+//}
+// para sumar la cantidad el total que me da los numeros que tengo en el array
+// for (let i = 0; i < numeros.length; i++) {
+//     //suma = suma + numeros[i]
+// }
+//     //console.log(suma)  con este mostramos la suma
+
+//     // para sacar los numeros pares del array
+
+//     function esPar(i) {
+//         if (i % 2 == 0) {
+//           return true;
+//         } else {
+//           return false;
+//         }
+//       }
+    
+//       for (let i = 0; i < numeros.length; i++) {
+//         if (esPar(numeros[i]) == true) {
+//          // console.log(numeros[i]); // asi mostramos los numeros pares
+//          } 
+//       }
+
+//       // para mostrar los numeros impares
+
+//       for (let i = 0; i < numeros.length; i++) {
+//         if (esPar(numeros[i]) == false) {
+//          // console.log(numeros[i]); con este mostramos los impares 
+//          } 
+//       }
+
+//       // sumando los numeros pares del array
+
+//       for (let i = 0; i < numeros.length; i++) {
+//         if (esPar(numeros[i]) == true) {
+//           //console.log(numeros[i]);
+//           suma =  suma + numeros[i]
+//       //    console.log(suma) con este mostramos la suma de los pares
+//         } 
+//       }
+
+     // sumando los numeros impares del array
+
+    //   for (let i = 0; i < numeros.length; i++) {
+    //     if (esPar(numeros[i]) == false) {
+    //       suma =  suma + numeros[i]
+    //     //  console.log(suma)  con este mostramos la suma de los imopares
+    //     } 
+    //   }
+
+      // para sacar los multiplos de 3 de el array
+
+    //   function esPar(i) {
+    //     if (i % 3 == 0) {
+    //       return true;
+    //     } else {
+    //       return false;
+    //     }
+    //   }
+    
+    //        for (let i = 0; i < numeros.length; i++) {
+    //         if (esPar(numeros[i]) == true) {
+    //            //console.log(numeros[i]); con este mostramos los multiplos de tres
+               
+    //           } 
+    //        }
+     // para sumar los multiplos de 3
+
+    //  function esPar(i) {
+    //     if (i % 3 == 0) {
+    //       return true;
+    //     } else {
+    //       return false;
+    //     }
+    //   }
+    
+    //        for (let i = 0; i < numeros.length; i++) {
+    //         if (esPar(numeros[i]) == true) {
+    //            suma = suma + numeros[i]
+    //            console.log(suma) //con este mostramos la suma de los multiplos de 3
+    //           } 
+    //        }
+
+          
+          // para sacar los multiplos de 5 del array
+
+        //    function esPar(i) {
+        //         if (i % 5 == 0) {
+        //           return true;
+        //         } else {
+        //           return false;
+        //         }
+        //       }
+            
+        //     for (let i = 0; i < numeros.length; i++) {
+        //          if (esPar(numeros[i]) == true) {
+        //         console.log(numeros[i]); //con este mostramos la suma de los multiplos de 5
+                       
+        //      } 
+        // }
+
+    // para sumar los multiplos de 5
+
+    // function esPar(i) {
+    //         if (i % 5 == 0) {
+    //           return true;
+    //         } else {
+    //           return false;
+    //         }
+    //       }
+        
+    //            for (let i = 0; i < numeros.length; i++) {
+    //             if (esPar(numeros[i]) == true) {
+    //                suma = suma + numeros[i]
+    //                console.log(suma) //con este mostramos la suma de los multiplos de 5
+    //               } 
+    //            }
