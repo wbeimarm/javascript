f5559d096e7d5345a0446336e25fcedc9ab3b2e0
wey-monsalve <wmarmonsa@gmail.com>
2022-11-10 13:03:23 -0500

    iniciando con arreglos

---
+++ arreglos.js
@@ -0,0 +1,13 @@
+//para mostrar cantidad de numeros del array
+
+let numeros =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+let i = numeros.length
+let suma = 0
+
+//console.log(i)  
+
+//para mostrar cada numero del array
+
+for (let i = 0; i < numeros.length; i++) {
+   console.log(numeros[i])  
+}
