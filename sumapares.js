e8d917b1792d2b834489b718a04338f320ee2514
wey-monsalve <wmarmonsa@gmail.com>
2022-11-10 13:31:20 -0500

    hago suma de numeros pares

---
+++ sumapares.js
@@ -0,0 +1,20 @@
+ // sumando las pares del arreglo
+ 
+let numeros =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+let i = numeros.length
+let suma = 0
+
+function esPar(i) {
+            if (i % 2 == 0) {
+              return true;
+            } else {
+              return false;
+            }
+          }
+
+ for (let i = 0; i < numeros.length; i++) {
+    if (esPar(numeros[i]) == true) {
+      suma =  suma + numeros[i]
+      console.log(suma) 
+    } 
+ } 
